3f0a496b36e70dbbf9109a1ce2700cac1d8eba43
Rajan chavda <www.chavdarajan@gmail.com>
2024-07-07 02:42:49 +0530

    check baseurl

--- src/service/webSocketService.ts
+++ src/service/webSocketService.ts
@@ -3,6 +3,8 @@ import { Task } from '../data/index.js';
 import { Ids as Keys } from '../TaskManager.js';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "";
+console.log("checkkkk", BASE_URL);
+
 
 const socket = io(BASE_URL , {
   withCredentials: true,
